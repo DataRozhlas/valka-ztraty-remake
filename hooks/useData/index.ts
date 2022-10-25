7a7ac62f7f00be2ab306deee85d8f5fb928bad07
Petr Kočí <petr.koci@samizdat.cz>
2022-10-25 20:30:25 +0200

    processed data

--- hooks/useData/index.ts
+++ hooks/useData/index.ts
@@ -6,6 +6,19 @@ const fetchData = async (url: string) => {
   if (url.slice(-3) === "csv") {
     const raw = await ky.get(url).text();
     const result = csvParse(raw);
+    // const translatedResult = await result.map(item => {
+    //   const translatedType = slovnicek.filter(
+    //     s => s[0] === item.equipment_type
+    //   )[0][1];
+    //   return {
+    //     country: item.country,
+    //     type: translatedType,
+    //     destroyed: Number(item.destroyed),
+    //     abandoned: Number(item.abandoned),
+    //     damaged: Number(item.damaged),
+    //     captured: Number(item.captured),
+    //   };
+    // });
     return result;
   }
   if (url.slice(-4) === "json") {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useData } from "../hooks";
+import { useData, dictionary } from "../hooks";
+import { useQuery } from "@tanstack/react-query";
+
+const translate = (string: string) => {
+  const result = dictionary.find(word => {
+    return word[0] === string;
+  });
+  if (typeof result === "undefined") console.log(string);
+  return result ? result[1] : string;
+};
 
 const Home: NextPage = () => {
-  const data = useData(
+  const { data, isSuccess }: any = useData(
     "https://data.irozhlas.cz/oryx-cache/totals_by_type.csv",
     "data"
   );
   const updated = useData(
     "https://data.irozhlas.cz/oryx-cache/updated.json",
     "updated"
+  );
+
+  const processed = useQuery(
+    ["processed"],
+    () => {
+      return data.map((item: { equipment_type: string }) => {
+        return {
+          ...item,
+          type: translate(item.equipment_type),
+        };
+      });
+    },
+    { enabled: isSuccess }
   );
 
   return <div>ahoj</div>;
